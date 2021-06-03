1cd3bba715bbe3822f851dc64d114cc701de4d4b
박민재\te 0610 <minjae2264@naver.com>
2021-06-03 23:09:35 +0900

    폭탄 사진 추가 (점수 깎이는거 10점) 스크립트 추가 / ground > ground 3 이미지 변경 / 맵 1단계 , 3단계 조절

---
+++ Assets/BombCoin.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombCoin : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Score.coinAmount += -10;//5점씩 증가
+        Destroy(gameObject);//동전아 사라져랏!
+        Sound.SoundPlay();
+
+        if (collision.gameObject.tag == "Co")//코인들한테 Co태그 줬거든요
+        {
+            collision.gameObject.SetActive(false);
+        }
+    }
+}
